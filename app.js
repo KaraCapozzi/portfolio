3071718b47ee960cb2f5e1b80ca7765f38c0350c
Kara Capozzi <karacapozzi@gmail.com>
2018-03-14 23:20:44 -0700

    nav bar, resume, some content for about me

---
+++ app.js
@@ -0,0 +1,13 @@
+// document.body.style.fontFamily = "Arial, sans-serif"
+// document.getElementById('nickname').innerText = ""
+// document.getElementById('nickname').innerHTML = '';
+// document.getElementById('favorites').innerHTML = '';
+// document.getElementById('hometown').innerHTML = 'Buffalo, NY';
+// var items = document.getElementsByTagName('li');
+// for (var i = 0; i < items.length; i++) {
+//    items[i].className = 'listitem';
+// }
+//
+//  var myPic = document.createElement('img');
+//  myPic.src = 'http://paidandpretty.com/wp-content/uploads/2017/03/Karas-Head.jpg';
+//  document.body.appendChild(myPic);
